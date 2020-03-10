0f38b51af015d4e719eb413e33883cbecb4ff509
AlVincentM <alvincentviar.musa@bicol-u.edu.ph>
2020-03-10 22:23:05 +0800

    taeng division yan

--- SumOfTwoDigits.asm
+++ SumOfTwoDigits.asm
@@ -1,18 +1,48 @@
 %include "asm_io.inc"
 
-segment .data
+	segment .data
 num1 db 5
-num2 db 4
+num2 db 5
+addMsg db "Addition: 5 + 5 = ", 0
+subMsg db "Subraction:  5 - 5 = ", 0
+mulMsg db "Multiplication:  5 * 5 = ", 0
+divMsg db "Division:  5 / 5 = ", 0
 
-segment .text
-	global _asm_main
+	segment .text
+global _asm_main
 
-_asm_main:
+	_asm_main:
 enter 0,0
 pusha
-mov eax, 4 	; give eax a value of 4
-add eax, 5	; add 5 to eax ex. 4 + 5
-call print_int ; print the value of eax. 9
+
+mov eax, addMsg
+call print_string
+mov eax, 5
+add eax, 5
+call print_int
+call print_nl
+
+mov eax, subMsg
+call print_string
+mov eax, 5
+sub eax, 5
+call print_int
+call print_nl
+
+mov eax, mulMsg
+call print_string
+mov eax, 5
+imul eax, 5
+call print_int
+call print_nl
+
+mov eax, divMsg
+call print_string
+mov eax, 5
+idiv eax, 1
+;call print_int
+;call print_nl
+
 popa
 mov eax,0
 leave
